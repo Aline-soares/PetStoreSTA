378d364976214a82e992382d195adeef50565fe5
Aline-soares <alinesoares.tur@gmail.com>
2023-05-28 13:18:11 +0100

    Finalizado o script excluirPet (Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -24,7 +24,7 @@ public class Pet {
     }
 
     // Incluir - Create - Post
-    @Test // Identifica o Método ou Função como um teste para o TestNG
+    @Test(priority = 1) // Identifica o Método ou Função como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
@@ -50,7 +50,7 @@ public class Pet {
 
     }
 
-    @Test
+    @Test(priority = 2)
     public void consultarPet(){
         String petId = "20210324";
 
@@ -65,10 +65,47 @@ public class Pet {
                 .body("name", is("Agnes"))
                 .body("category.name", is("cat"))
                 .body("status", is("available"))
-
+        .extract()
+                .path("category.name")
         ;
 
-
-
     }
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Agnes"))
+                .body("status", is("sold"))
+        ;
+    }
+
+    @Test (priority = 4)
+    public void excluirPet(){
+        String petId = "20210324";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+
+
+        ;
+    }
+
 }
